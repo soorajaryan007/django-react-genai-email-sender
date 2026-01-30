41d4b93a490b1df99221dca049c40c20563de9c3
sooraj aryan <soorajaryan007@gmail.com>
2026-01-31 03:26:54 +0530

    did some modification in urls and views

--- backend_django/emails/views.py
+++ backend_django/emails/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
-import asyncio
 from asgiref.sync import sync_to_async
 
 from .serializers import (
